d2101598bf49e1f919837a909572bb5aacf21a2c
Kishor Patidar <nkpatidar7987@gmail.com>
2021-08-05 11:28:12 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'students/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources  :students
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
+
+end
